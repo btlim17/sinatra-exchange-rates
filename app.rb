904b16240ddeaf1d2de661b9bf3c70954af2ee79
btlim17 <146046964+btlim17@users.noreply.github.com>
2023-10-25 01:25:44 +0000

    update2

--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  @currencies = parsed_data.fetch("currencies")
+  erb(:home)
+end
+
+get("/:from_currency") do
+  @og_currency = params.fetch("from_currency")
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  @currencies = parsed_data.fetch("currencies")
+  erb(:flex_og_currency)
+end
+
+get("/:from_currency/:to_currency") do
+  @og_currency = params.fetch("from_currency")
+  @new_currency = params.fetch("to_currency")
+  api_url = "https://api.exchangerate.host/convert?access_key=#{xrate_key}&from=#{@og_currency}&to=#{@to_currency}&amount=1"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+  parsed_data = JSON.parse(raw_data_string)
+  @conversion_rate = parsed_data.fetch("result")
 end
