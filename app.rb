ff3c7920a685d63ecb5335aceecda42cca203e4c
btlim17 <146046964+btlim17@users.noreply.github.com>
2023-10-25 01:31:03 +0000

    update4

--- app.rb
+++ app.rb
@@ -25,9 +25,12 @@ end
 get("/:from_currency/:to_currency") do
   @og_currency = params.fetch("from_currency")
   @new_currency = params.fetch("to_currency")
-  api_url = "https://api.exchangerate.host/convert?access_key=#{xrate_key}&from=#{@og_currency}&to=#{@to_currency}&amount=1"
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@og_currency}&to=#{@new_currency}&amount=1"
   raw_data = HTTP.get(api_url)
   raw_data_string = raw_data.to_s
   parsed_data = JSON.parse(raw_data_string)
+  #pp parsed_data
   @conversion_rate = parsed_data.fetch("result")
+ # pp @conversion_rate
+  erb(:flex_new_currency)
 end
